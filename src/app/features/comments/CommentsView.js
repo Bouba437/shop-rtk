63e49357c3afe2b9d59a4f4d17d6dd8974f7ddc5
Bouba <diarraboubaca@yahoo.fr>
2023-06-25 17:28:23 +0200

    ajout des commentaires avec createAsyncThunk

---
+++ src/app/features/comments/CommentsView.js
@@ -0,0 +1,34 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchComments } from "./commentsSlice";
+
+const CommentsView = () => {
+
+    const commentSlice = useSelector(state => state.comment);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(fetchComments()); // une fonction de type: AsyncThunkAction
+    }, [fetchComments]);
+
+    return (
+        <>
+            <h2>Commentaires</h2>
+            {
+                commentSlice.isLoading ? <p>Veuillez patienter ...</p>
+                : commentSlice.error ? <p style={{ color: "red", textAlign: "center" }}>{ commentSlice.error }</p> 
+                : commentSlice.comments.map(({body, email, id, name}) => {
+                    return (
+                        <div className="comments" key={id}>
+                            <p>Nom: { name }</p>
+                            <p>Email: { email }</p>
+                            <p>Avis: { body }</p>
+                        </div>
+                    )
+                })
+            }
+        </>
+    )
+}
+
+export default CommentsView
--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import phonesReducer from "./features/phones/phoneSlice";
 import tvsReducer from "./features/tvs/tvSlice";
+import commentsReducer from "./features/comments/commentsSlice"
 
 const store = configureStore({
     reducer: {
         phone: phonesReducer,
         television: tvsReducer,
+        comment: commentsReducer,
     }
 })
 
